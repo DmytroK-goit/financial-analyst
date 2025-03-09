dc91c9d31263efe86c9d8257edd7e8c80c1c8fda
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2025-03-09 11:08:01 +0200

    add shiny text

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom";
 import s from "./HomePage.module.css";
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../redux/UserAuth/selectors";
+import ShinyText from "../components/ShinyText/ShinyText";
 
 const HomePage = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return (
     <div className={s.container}>
-      <h1>Finance App</h1>
+      <ShinyText disabled={false} speed={3} className="custom-class">
+        <h1>My Financial Control</h1>
+      </ShinyText>
+
       <div className={s.contButt}>
         {!isLoggedIn && (
           <>
