d7443c3706cdfe48699263e5a8086eb746d5ac6a
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2025-03-22 17:00:33 +0200

    add style Notfound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,17 @@
+import s from "./NotFound.module.scss";
 const NotFound = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center w-screen">
-      <img
-        className="w-full"
+    <div className={s.cont}>
+      <a className={s.home_button} href="/">
+        Додому
+      </a>
+      {/* <img
         src="https://internetdevels.com/sites/default/files/public/blog_preview/404_page_cover.jpg"
-      />
+        alt="404 Not Found"
+        className={s.img}
+      /> */}
     </div>
   );
 };
+
 export default NotFound;
